320202f35f746e38982bd844b3c5bee4a12654ac
Blue-Kite <bluelemon112@gmail.com>
2024-04-13 14:32:03 +0900

    [chore] 초기세팅

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { Theme } from "@radix-ui/themes";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <Theme>
+    <App />
+  </Theme>
+);
